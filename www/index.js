067d5dee76a1048a8627f29741e9341a43bc67a7
Craig Mayhew <craig@mayhew.io>
2019-07-21 18:48:22 +0100

    Changed to use "app" id

--- www/index.js
+++ www/index.js
@@ -2,7 +2,7 @@ import * as wasm from "BigPrimes";
 
 window.onload = function() {
     
-    var elBody = document.getElementById("content");
+    var elBody = document.getElementById("app");
     //todo lets do our routing in JS
 
     //home page
