db36830bad0be16487df99587f6e8fd4cf0c1e01
Craig Mayhew <craig@mayhew.io>
2019-07-22 13:18:41 +0100

    Removed redundent js onready

--- www/index.js
+++ www/index.js
@@ -1,10 +1,1 @@
 import * as wasm from "BigPrimes";
-
-window.onload = function() {
-    
-    var elBody = document.getElementById("app");
-    //todo lets do our routing in JS
-
-    //home page
-    elBody.innerHTML = wasm.page_index();
-}
