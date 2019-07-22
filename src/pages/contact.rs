65d5534e733f9fb337ed9c670c7f5556589cd950
Craig Mayhew <craig@mayhew.io>
2019-07-22 18:54:40 +0100

    Added contact links to contact page

--- src/pages/contact.rs
+++ src/pages/contact.rs
@@ -4,5 +4,14 @@ use crate::Msg;
 pub fn render() -> seed::dom_types::Node<Msg> {
     div![
         h1!["Contact"],
+        br![],
+        br![],
+        br![],
+        span!["Please contact me via ",
+            a!["twitter", attrs!{At::Class => "link", At::Href => "https://twitter.com/craigmayhew"}],
+            " or by opening an issue on ",
+            a!["github", attrs!{At::Class => "link", At::Href => "https://github.com/craigmayhew/bigprimes.net/"}],
+            ".",
+        ],
     ]
 }
