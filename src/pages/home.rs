351b48a8d987fd90ff82880bc2460a9424615b81
Craig Mayhew <craig@mayhew.io>
2019-07-22 10:18:58 +0100

    snake case news_items

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -7,14 +7,14 @@ pub mod home {
     }
 
     fn changeFuncName() -> [NewsItem; 1] {
-        let aNewsItems: [NewsItem; 1] = [
+        let news_items: [NewsItem; 1] = [
             NewsItem{
                 title: String::from(
                     "<a class=\"link\" href=\"https://github.com/craigmayhew/ethereum-contracts\">Ethereum bounty contracts</a>have now been added for anyone that would like to try to push the boundaries of human knowledge. The first contract is a competition for the correct solution to a³+b³+c³ = 33. Tweet me if you need assistance using these contracts!"),
                 date: NaiveDate::from_ymd(2019, 1, 22)
             }
         ];
-        aNewsItems
+        news_items
     }
 
 }
