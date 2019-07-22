52d57d7b3cc7661ab1c962bf823fbaee7afeb6e9
Craig Mayhew <craig@mayhew.io>
2019-07-22 10:14:59 +0100

    Example fn now returns array of NewsItem structs

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -6,12 +6,15 @@ pub mod Home {
         pub date: NaiveDate,
     }
 
-    fn changeFuncName() -> NewsItem {
-        let item :NewsItem = NewsItem{
-                                title: String::from("<a class=\"link\" href=\"https://github.com/craigmayhew/ethereum-contracts\">Ethereum bounty contracts</a> have now been added for anyone that would like to try to push the boundaries of human knowledge. The first contract is a competition for the correct solution to a³+b³+c³ = 33. Tweet me if you need assistance using these contracts!"),
-                                date: NaiveDate::from_ymd(2019, 1, 22)
-                            };
-        item
+    fn changeFuncName() -> [NewsItem; 1] {
+        let aNewsItems: [NewsItem; 1] = [
+            NewsItem{
+                title: String::from(
+                    "<a class=\"link\" href=\"https://github.com/craigmayhew/ethereum-contracts\">Ethereum bounty contracts</a>have now been added for anyone that would like to try to push the boundaries of human knowledge. The first contract is a competition for the correct solution to a³+b³+c³ = 33. Tweet me if you need assistance using these contracts!"),
+                date: NaiveDate::from_ymd(2019, 1, 22)
+            }
+        ];
+        aNewsItems
     }
 
 }
