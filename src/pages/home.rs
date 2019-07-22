d054c98b9635c82a47f22fa805f6524f684e01dd
Craig Mayhew <craig@mayhew.io>
2019-07-22 19:10:59 +0100

    Attempting to render html in news item

--- src/pages/home.rs
+++ src/pages/home.rs
@@ -2,18 +2,27 @@ use crate::Msg;
 use chrono::{NaiveDate};
 use seed::prelude::*;
 
-const NUM_NEWS_ITEMS :usize = 1;
-
 struct NewsItem {
-    pub content: String,
+    pub content: Vec<Node<Msg>>,
     pub date: NaiveDate,
 }
 
+const NUM_NEWS_ITEMS :usize = 2;
 fn get_news_items() -> [NewsItem; NUM_NEWS_ITEMS] {
     let news_items: [NewsItem; NUM_NEWS_ITEMS] = [
         NewsItem{
-            content: String::from(
-                "<a class=\"link\" href=\"https://github.com/craigmayhew/ethereum-contracts\">Ethereum bounty contracts</a>have now been added for anyone that would like to try to push the boundaries of human knowledge. The first contract is a competition for the correct solution to a³+b³+c³ = 33. Tweet me if you need assistance using these contracts!"),
+            content: vec![
+                a!["Ethereum bounty contracts", attrs!{At::Class => "link", At::Href => "https://github.com/craigmayhew/ethereum-contracts"}],
+                span!["have now been added for anyone that would like to try to push the boundaries of human knowledge. 
+                The first contract is a competition for the correct solution to a³+b³+c³ = 33. 
+                Tweet me if you need assistance using these contracts!"]
+            ],
+            date: NaiveDate::from_ymd(2019, 1, 22)
+        },
+        NewsItem{
+            content: vec![
+                div!["huh"]
+            ],
             date: NaiveDate::from_ymd(2019, 1, 22)
         }
     ];
@@ -27,7 +36,7 @@ pub fn render() -> seed::dom_types::Node<Msg> {
         let news_item = &news_items[i];
         table_rows[i] = tr![
             td![news_item.date.to_string()],
-            td![news_item.content]
+            td![news_item.content.to_vec()]
         ];
     };
     
