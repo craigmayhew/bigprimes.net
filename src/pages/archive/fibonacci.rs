a2e1d9c80e53a125aacb5fda242a80f30a6afc20
Craig Mayhew <craig@mayhew.io>
2019-07-23 15:23:18 +0100

    Added an nth() formatting function

--- src/pages/archive/fibonacci.rs
+++ src/pages/archive/fibonacci.rs
@@ -1,11 +1,23 @@
 use seed::prelude::*;
 use crate::Msg;
 
+fn nth(num: isize) -> String {
+    format!("{}{}", num, match (num % 10, num % 100) {
+        (1, 11) | (2, 12) | (3, 13) => "th",
+        (1, _) => "st",
+        (2, _) => "nd",
+        (3, _) => "rd",
+        _ => "th",
+    })
+}
+
 pub fn render(slug:String) -> seed::dom_types::Node<Msg> {
+    let slug_int:isize = slug.parse().unwrap();
+
     div![
         h1!["The Fibonacci Numbers"],
         br![],
-        "This page shows the ",slug," fibonacci number followed by the next 0.",
+        "This page shows the ",nth(slug_int)," fibonacci number followed by the next 0.",
         br![],
         br![],
         a!["previous 25 fibonacci numbers", attrs!{At::Class => "link", At::Href => "/archive/fibonacci/70332/"}],
