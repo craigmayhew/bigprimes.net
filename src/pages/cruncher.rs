a6510f8a2aa26d47161bc6e4bb6a3e127fe2bfb7
Craig Mayhew <craig@mayhew.io>
2019-07-27 11:04:54 +0100

    Removed superfluous El::from_html()

--- src/pages/cruncher.rs
+++ src/pages/cruncher.rs
@@ -516,7 +516,7 @@ fn html_crunched_number(slug:String) -> seed::dom_types::Node<Msg> {
                         "Egyptian Numerals:",
                     ],
                     td![
-                        El::from_html(&numerics_to_text::den_to_egyptian(&slug)),
+                        numerics_to_text::den_to_egyptian(&slug),
                     ]
                 ],
                 tr![
