22a140d5f4c1ad483722ec36401e4ef88f1dfa2c
Craig Mayhew <craig@mayhew.io>
2019-08-25 11:26:08 +0100

    Completed refactor for remaining 30 mersennes. Many less lines of code.

--- src/pages/archive/mersenne.rs
+++ src/pages/archive/mersenne.rs
@@ -33,6 +33,36 @@ pub mod mersenne_utils {
             vec!["8-Sep-1957"],		
             vec!["3-Nov-1961"],
             vec!["3-Nov-1961"],
+            vec!["11-May-1963"],
+            vec!["16-May-1963"],
+            vec!["2-Jun-1963"],
+            vec!["4-Mar-1971"],
+            vec!["30-Oct-1978"],
+            vec!["9-Feb-1979"],
+            vec!["8-Apr-1979"],
+            vec!["25-Sep-1982"],
+            vec!["28-Jan-1988"],
+            vec!["20-Sep-1983"],
+            vec!["6-Sep-1985"],
+            vec!["19-Feb-1992"],
+            vec!["10-Jan-1994"],
+            vec!["3-Sep-1996"],
+            vec!["12-Nov-1996"],
+            vec!["24-Aug-1997"],
+            vec!["27-Jan-1998"],
+            vec!["1-Jun-1999"],
+            vec!["14-Nov-2001"],
+            vec!["17-Nov-2003"],
+            vec!["28-May-2004"],
+            vec!["26-Feb-2005"],
+            vec!["15-Dec-2005"],
+            vec!["4-Sep-2006"],
+            vec!["23-Aug-2008"],
+            vec!["06-Sep-2009"],
+            vec!["12-Apr-2008"],
+            vec!["25-Jan-2013"],
+            vec!["07-Jan-2016"],
+            vec!["26-Dec-2017"],
         ];
 
         mersennes_discovery_dates[n][0].to_owned()
@@ -62,7 +92,37 @@ pub mod mersenne_utils {
             vec![2281, 687],
             vec![3217, 969],		
             vec![4253,1281],
-            vec![4423,1332],	
+            vec![4423,1332],
+            vec![9689,2917],
+            vec![9941,2993],
+            vec![11213,3376],
+            vec![19937,6002],
+            vec![21701,6533],
+            vec![23209,6987],
+            vec![44497,13395],
+            vec![86243,25962],
+            vec![110503,33265],
+            vec![132049,39751],
+            vec![216091,65050],
+            vec![756839,227832],
+            vec![859433,258716],
+            vec![1257787,378632],
+            vec![1398269,420921],
+            vec![2976221,895832],
+            vec![3021377,909526],
+            vec![6972593,2098960],
+            vec![13466917,4053946],
+            vec![20996011,6320430],
+            vec![24036583,7235733],
+            vec![25964951,7816230],
+            vec![30402457,9152052],
+            vec![32582657,9808358],
+            vec![37156667,11185272],
+            vec![42643801,12837064],
+            vec![43112609,12978189],
+            vec![57885161,17425170],
+            vec![74207281,22338618],
+            vec![77232917,23249425],
         ]
     }
 
@@ -136,289 +196,4 @@ pub fn render() -> seed::dom_types::Node<Msg> {
             ]
         ]
     ]
-
-    /*div![
-<tr>
- <td>50</td>
- <td>2<sup>77232917</sup>-1</td>
- <td>23249425</td>
- <td>26-Dec-2017</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M50.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>49</td>
- <td>2<sup>74207281</sup>-1</td>
- <td>22338618</td>
- <td>07-Jan-2016</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M49.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>48</td>
- <td>2<sup>57885161</sup>-1</td>
- <td>17425170</td>
- <td>25-Jan-2013</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M48.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>47</td>    
- <td>2<sup>43112609</sup>-1</td>
- <td>12978189</td>
- <td>12-Apr-2008</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M47.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>46</td>    
- <td>2<sup>42643801</sup>-1</td>
- <td>12837064</td>
- <td>06-Sep-2009</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M46.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>45</td>    
- <td>2<sup>37156667</sup>-1</td>
- <td>11185272</td>
- <td>23-Aug-2008</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M45.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>44</td>	
- <td>2<sup>32582657</sup>-1</td>
- <td>9808358</td>
- <td>4-Sep-2006</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M44.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>43</td>
- <td>2<sup>30402457</sup>-1</td>
- <td>9152052</td>
- <td>15-Dec-2005</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M43.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>42</td>
- <td>2<sup>25964951</sup>-1</td>
- <td>7816230</td>
- <td>26-Feb-2005</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M42.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>41</td>
- <td>2<sup>24036583</sup>-1</td>
- <td>7235733</td>
- <td>28-May-2004</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M41.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>40</td>
- <td>2<sup>20996011</sup>-1</td>
- <td>6320430</td>
- <td>17-Nov-2003</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M40.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>39</td>
- <td>2<sup>13466917</sup>-1</td>
- <td>4053946</td>
- <td>14-Nov-2001</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M39.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>38</td>
- <td>2<sup>6972593</sup>-1</td>
- <td>2098960</td>
- <td>1-Jun-1999</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M38.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>37</td>
- <td>2<sup>3021377</sup>-1</td>
- <td>909526</td>
- <td>27-Jan-1998</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M37.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>36</td>
- <td>2<sup>2976221</sup>-1</td>
- <td>895832</td>
- <td>24-Aug-1997</td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M36.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>35</td>
- <td>2<sup>1398269</sup>-1</td>
- <td>420921</td>
- <td>12-Nov-1996</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M35.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M35.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>34</td>
- <td>2<sup>1257787</sup>-1</td>
- <td>378632</td>
- <td>3-Sep-1996</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M34.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M34.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>33</td>
- <td>2<sup>859433</sup>-1</td>
- <td>258716</td>
- <td>10-Jan-1994</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M33.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M33.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>32</td>
- <td>2<sup>756839</sup>-1</td>
- <td>227832</td>
- <td>19-Feb-1992</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M32.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M32.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>31</td>
- <td>2<sup>216091</sup>-1</td>
- <td>65050</td>
- <td>6-Sep-1985</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M31.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M31.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>30</td>
- <td>2<sup>132049</sup>-1</td>
- <td>39751</td>
- <td>20-Sep-1983</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M30.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M30.zip\">ZIP</a></td>
-</tr>
-<tr>
- <td>29</td>
- <td>2<sup>110503</sup>-1</td>
- <td>33265</td>
- <td>28-Jan-1988</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M29.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>28</td>
- <td>2<sup>86243</sup>-1</td>
- <td>25962</td>
- <td>25-Sep-1982</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M28.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>27</td>
- <td>2<sup>44497</sup>-1</td>
- <td>13395</td>
- <td>8-Apr-1979</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M27.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>26</td>
- <td>2<sup>23209</sup>-1</td>
- <td>6987</td>
- <td>9-Feb-1979</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M26.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>25</td>
- <td>2<sup>21701</sup>-1</td>
- <td>6533</td>
- <td>30-Oct-1978</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M25.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>24</td>
- <td>2<sup>19937</sup>-1</td>
- <td>6002</td>
- <td>4-Mar-1971</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M24.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>23</td>
- <td>2<sup>11213</sup>-1</td>
- <td>3376</td>
- <td>2-Jun-1963</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M23.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>22</td>
- <td>2<sup>9941</sup>-1</td>
- <td>2993</td>
- <td>16-May-1963</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M22.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>21</td>
- <td>2<sup>9689</sup>-1</td>
- <td>2917</td>
- <td>11-May-1963</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M21.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
- <td>20</td>
- <td>2<sup>4423</sup>-1</td>
- <td>1332</td>
- <td>3-Nov-1961</td>
- <td width=\"30\"><a href=\"https://static.bigprimes.net/archive/mersenne/M20.txt\">TXT</a></td>
- <td width=\"30\"></td>
- <td width=\"30\"></td>
-</tr>
-<tr>
-</tr>
-</tbody></table>"),
-    ]*/
 }
