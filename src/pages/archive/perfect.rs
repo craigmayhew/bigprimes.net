df88adfb60da0f3fcf6a986b4b7a721646b8e7e3
Craig Mayhew <craig@mayhew.io>
2019-10-13 18:46:45 +0100

    Correcting table layout when TXT download link appears

--- src/pages/archive/perfect.rs
+++ src/pages/archive/perfect.rs
@@ -108,7 +108,7 @@ pub fn render(model: &crate::Model) -> seed::dom_types::Node<Msg> {
                 td![perfect.digits.to_string()],//digits in length
                 td![perfect.discovery],//discovery
 				if model.download.n == perfect.n {
-					perfects_utils::generate_file(model.download.n,model.download.p)
+					td![perfects_utils::generate_file(model.download.n,model.download.p)]
 				} else {
 					td![button!["Generate",mouse_ev("mouseup", move |event| Msg::GenerateDownload(event, perfect_download))]]
 				},
