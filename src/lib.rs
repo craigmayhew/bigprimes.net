2898e4e47b6ded37b5ab6a6a58b88f96e6c97d1f
Craig Mayhew <craig@mayhew.io>
2021-06-26 16:08:08 +0100

    Page routing works again for seed 0.8 upgrade

--- src/lib.rs
+++ src/lib.rs
@@ -32,21 +32,26 @@ pub struct Model {
     slug: std::string::String,
 }
 
-// Setup a default here, for initialization later.
-impl Default for Model {
-    fn default() -> Self {
+impl Model {
+    fn new(url: Url) -> Self {
+        let (page, slug) = routes(url.clone());
+
         Self {
             download: pages::archive::perfect::perfects_utils::PerfectDownload { n: 0, p: 0 },
-            page: Page::Home,
-            slug: "".to_owned(),
+            page: page,
+            slug: slug,
         }
     }
+}
+
+fn init(url: Url, orders: &mut impl Orders<Msg>) -> Model {
+    orders.subscribe(Msg::UrlChanged);
+    Model::new(url)
 }
 
 // Update
 #[derive(Clone)]
 pub enum Msg {
-    ChangePage(Page, std::string::String),
     GenerateMersenneDownload(
         web_sys::MouseEvent,
         pages::archive::mersenne::mersenne_utils::MersenneDownload,
@@ -55,18 +60,12 @@ pub enum Msg {
         web_sys::MouseEvent,
         pages::archive::perfect::perfects_utils::PerfectDownload,
     ),
+    UrlChanged(subs::UrlChanged),
 }
 
 /// The sole source of updating the model
-fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
+fn update(msg: Msg, model: &mut Model, orders: &mut impl Orders<Msg>) {
     match msg {
-        Msg::ChangePage(page, slug) => {
-            //reset download so subsequent visits to perfect page don't hang the browser with a download
-            model.download =
-                pages::archive::perfect::perfects_utils::PerfectDownload { n: 0, p: 0 };
-            model.page = page;
-            model.slug = slug
-        }
         Msg::GenerateMersenneDownload(_event, perfect_download) => {
             //event.prevent_default();
             model.download.n = perfect_download.n;
@@ -77,12 +76,20 @@ fn update(msg: Msg, model: &mut Model, _: &mut impl Orders<Msg>) {
             model.download.n = perfect_download.n;
             model.download.p = perfect_download.p
         }
+        Msg::UrlChanged(subs::UrlChanged(url)) => {
+            let (page, slug) = routes(url);
+            model.download =
+                pages::archive::perfect::perfects_utils::PerfectDownload { n: 0, p: 0 };
+            model.page = page;
+            model.slug = slug;
+            ()
+        }
     }
 }
 
 /// The top-level component we pass to the virtual dom. Must accept the model as its
 /// only argument, and output has to implement trait `ElContainer`.
-fn view(model: &Model) -> impl View<Msg> {
+fn view(model: &Model) -> Node<Msg> {
     match model.page {
         Page::ContactUs => pages::contact::render(),
         Page::Downloads => pages::downloads::render(),
@@ -99,74 +106,47 @@ fn view(model: &Model) -> impl View<Msg> {
     }
 }
 
-impl ToString for Page {
-    fn to_string(&self) -> String {
-        // Eg for url routing
-        match self {
-            Page::ContactUs => "contactus".into(),
-            Page::Downloads => "downloads".into(),
-            Page::Faq => "faq".into(),
-            Page::FermatArchive => "fermat".into(),
-            Page::FibonacciArchive => "fibonacci".into(), //TODO: check this is right, might need the archive/ prefix!
-            Page::Home => "home".into(),
-            Page::Status => "status".into(),
-            Page::MersenneArchive => "mersenne".into(),
-            Page::NumberCruncher => "cruncher".into(),
-            Page::PerfectArchive => "perfect".into(),
-            Page::PrimalityChecker => "primalitytest".into(),
-            Page::PrimeNumbersArchive => "primes".into(), //TODO: check this is right, might need the archive/ prefix!
-        }
-    }
-}
-
-fn routes(url: seed::Url) -> Option<Msg> {
+fn routes(url: seed::Url) -> (Page, std::string::String) {
     let empty_string = "".to_owned();
 
-    if url.path.is_empty() {
-        return Some(Msg::ChangePage(Page::Home, empty_string));
+    if url.path().is_empty() {
+        return (Page::Home, empty_string);
     }
 
-    Some(match url.path[0].as_ref() {
+    match url.path()[0].as_ref() {
         "archive" => {
             // Determine if we are at the archive page, or a subpage
-            match url.path[1].as_ref() {
-                "fermat" => Msg::ChangePage(Page::FermatArchive, empty_string),
-                "fibonacci" => match url.path.get(2).as_ref() {
-                    Some(_slug) => Msg::ChangePage(Page::FibonacciArchive, url.path[2].to_owned()),
-                    None => Msg::ChangePage(Page::FibonacciArchive, "1".to_owned()),
+            match url.path()[1].as_ref() {
+                "fermat" => (Page::FermatArchive, empty_string),
+                "fibonacci" => match url.path().get(2).as_ref() {
+                    Some(_slug) => (Page::FibonacciArchive, url.path()[2].to_owned()),
+                    None => (Page::FibonacciArchive, "1".to_owned()),
                 },
-                "mersenne" => Msg::ChangePage(Page::MersenneArchive, empty_string),
-                "perfect" => Msg::ChangePage(Page::PerfectArchive, empty_string),
-                "prime" => match url.path.get(2).as_ref() {
+                "mersenne" => (Page::MersenneArchive, empty_string),
+                "perfect" => (Page::PerfectArchive, empty_string),
+                "prime" => match url.path().get(2).as_ref() {
                     Some(_slug) => {
-                        Msg::ChangePage(Page::PrimeNumbersArchive, url.path[2].to_owned())
+                        (Page::PrimeNumbersArchive, url.path()[2].to_owned())
                     }
-                    None => Msg::ChangePage(Page::PrimeNumbersArchive, "1".to_owned()),
+                    None => (Page::PrimeNumbersArchive, "1".to_owned()),
                 },
-                _ => Msg::ChangePage(Page::Home, empty_string), //TODO: add archive page
+                _ => (Page::Home, empty_string), //TODO: add archive page
             }
         }
-        "contactus" => Msg::ChangePage(Page::ContactUs, empty_string),
-        "cruncher" => match url.path.get(1).as_ref() {
-            Some(_slug) => Msg::ChangePage(Page::NumberCruncher, url.path[1].to_owned()),
-            None => Msg::ChangePage(Page::NumberCruncher, empty_string),
+        "contactus" => (Page::ContactUs, empty_string),
+        "cruncher" => match url.path().get(1).as_ref() {
+            Some(_slug) => (Page::NumberCruncher, url.path()[1].to_owned()),
+            None => (Page::NumberCruncher, empty_string),
         },
-        "downloads" => Msg::ChangePage(Page::Downloads, empty_string),
-        "faq" => Msg::ChangePage(Page::Faq, empty_string),
-        "primalitytest" => Msg::ChangePage(Page::PrimalityChecker, empty_string),
-        "status" => Msg::ChangePage(Page::Status, empty_string),
-        _ => Msg::ChangePage(Page::Home, empty_string),
-    })
+        "downloads" => (Page::Downloads, empty_string),
+        "faq" => (Page::Faq, empty_string),
+        "primalitytest" => (Page::PrimalityChecker, empty_string),
+        "status" => (Page::Status, empty_string),
+        _ => (Page::Home, empty_string),
+    }
 }
 
 #[wasm_bindgen(start)]
-pub fn render() {
-    seed::App::build(|_, _| Init::new(Model::default()), update, view)
-        .routes(routes)
-        .build_and_start();
-}
-
-pub fn log(s: &str) {
-    use web_sys::console;
-    console::log_1(&s.into());
+pub fn start() {
+    App::start("app", init, update, view);
 }
