090e2c7fc98f2f351d75c52a8f5cc7f2f58f58f9
Craig Mayhew <craig@mayhew.io>
2021-07-10 15:30:58 +0100

    El::from_html() replaced with corresponding html macros

--- src/pages/primalitytest.rs
+++ src/pages/primalitytest.rs
@@ -9,35 +9,51 @@ pub fn render() -> Node<Msg> {
         br![],
         //TODO: Convert the j.js file into rust!
         Script!(attrs!{At::Src => "https://static.bigprimes.net/j.js"}),
-        El::from_html("
-            <table cellpadding=\"10\" cellspacing=\"0\" class=\"text\" width=\"200\" style=\"border:1px solid #444; background-color:#e0faed\">
-            <tr>
-            <td>
-            <form name=\"primetest\" onsubmit=\"return false\" action=\"\">
-            This test uses javascript and is limited to checking numbers upto 16 digits.<br /><br />
-            Is <input type=\"text\" size=\"16\" name=\"input\" value=\"\" maxlength=\"16\" /> prime? 
-            <input onclick=\"check(false,0)\" type=\"button\" value=\"Check!\" />
-            <br /><br />
-            <textarea name=\"javascriptoutput\" cols=\"40\" rows=\"2\" disabled=\"disabled\"></textarea>
-            </form>
-            </td>
-            </tr>
-            </table>
-            <br /><br />
-            <table bgcolor=\"#e0faed\" cellpadding=\"10\" cellspacing=\"0\" class=\"text\" width=\"200\" style=\"border:1px solid #444;\">
-            <tr>
-            <td>
-            <form name=\"primelist\" onsubmit=\"return false\" action=\"\">
-            This uses javascript and is limited to checking numbers upto 15 digits.<br /><br />
-            This will show <input size=\"4\" name=\"primes\" maxlength=\"2\" value=\"1\" />
-            prime numbers after <input size=\"16\" name=\"start\" maxlength=\"15\" value=\"0\" /> 
-            <input onclick=\"primelist.javascriptlistoutput.value='';listy();\" type=\"button\" value=\"Go!\" />
-            <br /><br />
-            <textarea id=\"javascriptlistoutput\" cols=\"60\" rows=\"10\" disabled=\"disabled\"></textarea>
-            </form>
-            </td>
-            </tr>
-            </table>
-        "),        
+        table![
+            attrs!{At::Class => "text", At::Width => "300", At::Style => "border:1px solid #444; background-color:#e0faed"},
+            tr![
+                td![
+                    attrs!{At::Style => "padding: 10px"},
+                    form![
+                        attrs!{At::Name => "primetest", At::OnSubmit => "return false"},
+                        "Tool is limited to checking numbers upto 16 digits.",
+                        br![],
+                        br![],
+                        "Is ",
+                        input![attrs!{At::Type => "text", At::Size => "16", At::Name => "input", At::Value => "", At::MaxLength => "16"}],
+                        " prime? ",
+                        button![attrs!{At::OnClick => "check(false,0)"},"Check!"],
+                        br![],
+                        br![],
+                        textarea![attrs!{At::Name => "javascriptoutput", At::Cols => 60, At::Rows => 2, At::Disabled => "disabled"}]
+                    ]
+                ]
+            ]
+        ],
+        br![],
+        br![],
+        table![
+            attrs!{At::Class => "text", At::Width => "300", At::Style => "border:1px solid #444; background-color:#e0faed"},
+            tr![
+                td![
+                    attrs!{At::Style => "padding: 10px"},
+                    form![
+                        attrs!{At::Name => "primelist", At::OnSubmit => "return false"},
+                        "This tool is limited to checking numbers upto 15 digits.",
+                        br![],
+                        br![],
+                        "This will show ",
+                        input![attrs!{At::Type => "text", At::Size => "4", At::Name => "primes", At::Value => "1", At::MaxLength => "2"}],
+                        " prime numbers after ",
+                        input![attrs!{At::Type => "text", At::Size => "16", At::Name => "start", At::Value => "0", At::MaxLength => "15"}],
+                        " ",
+                        button![attrs!{At::OnClick => "primelist.javascriptlistoutput.value='';listy();"},"Go!"],
+                        br![],
+                        br![],
+                        textarea![attrs!{At::Name => "javascriptlistoutput", At::Cols => 60, At::Rows => 10, At::Disabled => "disabled"}]
+                    ]
+                ]
+            ]
+        ]
     ]
 }
