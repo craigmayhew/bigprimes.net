9edb457efe999958905a4cc632612e46a0c89c39
Craig Mayhew <craig@mayhew.io>
2021-07-10 16:01:59 +0100

    Added trial_divide() and corresponding unit tests

--- src/pages/primalitytest.rs
+++ src/pages/primalitytest.rs
@@ -1,6 +1,36 @@
 use crate::Msg;
 use seed::prelude::*;
 
+fn trial_divide(n: usize, max: usize) -> usize {
+    // Trial divides the positive integer n by the primes from 2 to max
+    // Returns the first prime divisor found, or 0 if none found
+    // Note: if n < max^2 is a prime, then n will be returned.
+    if n % 2 == 0 {
+        2
+    } else if n % 3 == 0 {
+        3
+    } else {
+        // No need to go past the square root of our number
+        let sqrt: usize = (n as f64).sqrt().floor() as usize;
+        let stop = if sqrt > max { max } else { sqrt };
+        // Okay, lets "wheel factor" alternately adding 2 and 4
+        let mut di = 2;
+        let mut i = 5;
+        while i <= stop {
+            if n % i == 0 {
+                return i;
+            }
+            i += di;
+            di = 6 - di;
+        }
+        if n >= max * max {
+            0
+        } else {
+            n
+        }
+    }
+}
+
 pub fn render() -> Node<Msg> {
     div![
         h1!["Primality Test, Calculate Primes"],
@@ -65,4 +95,20 @@ pub fn render() -> Node<Msg> {
             ]]
         ]
     ]
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn trial_divide_test() {
+        assert_eq!(trial_divide(2, 2), 2);
+        assert_eq!(trial_divide(3, 4), 3);
+        assert_eq!(trial_divide(30, 10000), 2);
+        assert_eq!(trial_divide(7777771, 10000), 29);
+        assert_eq!(trial_divide(7777771111111111, 10000), 11);
+        //in the case of a prime
+        assert_eq!(trial_divide(777777111111113, 10000), 0);
+    }
 }
