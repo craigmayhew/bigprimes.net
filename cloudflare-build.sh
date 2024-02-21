9ad13f3efbd3dad053164e3e80dbac7293d55f75
Craig Mayhew <craig@mayhew.io>
2024-02-21 16:01:13 +0000

    Added release flag to deploy build

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -5,4 +5,4 @@ rustup target add wasm32-unknown-unknown
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 cargo +stable install --locked trunk
 cargo +stable install wasm-bindgen-cli
-trunk build
+trunk build --release
