45715fd91c9f1a862aae8e5458c2828e5488d32c
Craig Mayhew <craig@mayhew.io>
2024-02-21 11:10:05 +0000

    better spacing in question

--- src/pages/faq.rs
+++ src/pages/faq.rs
@@ -15,7 +15,7 @@ pub fn render() -> Node<Msg> {
         p!["The first occurrence is between the prime numbers 1671781 and 1671907 on ",a!["this", attrs!{At::Class => "link", At::Href => "/archive/prime/1262/"}]," page (near bottom of 3rd column)."],
         b!["What is the smallest prime number that contains all digits 0 to 9 in sequence?"],
         p!["100123456789"],
-        b!["What percentage of prime numbers end in 1,3,7 and 9?"],
+        b!["What percentage of prime numbers end in 1, 3, 7 and 9?"],
         p!["25% of prime numbers end in a 1",br![],"25% of prime numbers end in a 3",br![],"25% of prime numbers end in a 7",br![],"25% of prime numbers end in a 9"],        
     ]
 }
