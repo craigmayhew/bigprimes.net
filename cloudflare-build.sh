d19765955f78fb65c4b9480b689986ca458795c2
Craig Mayhew <craig@mayhew.io>
2024-02-21 10:16:38 +0000

    wasm pack moved after default nightly and target change for further debug

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -1,8 +1,8 @@
 curl https://sh.rustup.rs -sSf | sh -s -- -y
 . $HOME/.cargo/env
-curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 rustup default nightly
 rustup target add wasm32-unknown-unknown
+curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 cargo install --locked trunk
 cargo install wasm-bindgen-cli
 trunk build
