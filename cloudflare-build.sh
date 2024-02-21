6fc4577d91bc11d308f6d43c62dfb18fc5b19fac
Craig Mayhew <craig@mayhew.io>
2024-02-21 09:24:37 +0000

    Trunk compiles rather than bininstalls during CI build

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -4,6 +4,7 @@ curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
 rustup target add wasm32-unknown-unknown
 #cargo install cargo-binstall
 #cargo-binstall --locked --no-confirm trunk
+cargo install --locked trunk
 cargo install wasm-bindgen-cli
 rustup default nightly
 trunk build
