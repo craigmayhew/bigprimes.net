3f907ec5a756c2141caeca62a960ed9ea6457f36
Craig Mayhew <craig@mayhew.io>
2024-02-21 11:10:28 +0000

    Corrected link to specific prime page

--- src/pages/faq.rs
+++ src/pages/faq.rs
@@ -12,7 +12,7 @@ pub fn render() -> Node<Msg> {
         b!["I've found a bug/mistake on bigprimes.net!"],
         p!["Please opening an issue on ",a!["github", attrs!{At::Class => "link", At::Href => "https://github.com/craigmayhew/bigprimes.net/"}]],
         b!["What is the first set of 100 numbers (e.g. 100-199, 1300-1399, 312300-312399) that does not contain a prime number?"],
-        p!["The first occurrence is between the prime numbers 1671781 and 1671907 on ",a!["this", attrs!{At::Class => "link", At::Href => "/archive/prime/1262/"}]," page (near bottom of 3rd column)."],
+        p!["The first occurrence is between the prime numbers 1671781 and 1671907 on ",a!["this", attrs!{At::Class => "link", At::Href => "/archive/prime/126100/"}]," page (near bottom of 3rd column)."],
         b!["What is the smallest prime number that contains all digits 0 to 9 in sequence?"],
         p!["100123456789"],
         b!["What percentage of prime numbers end in 1, 3, 7 and 9?"],
