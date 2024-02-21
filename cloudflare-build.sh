fea88d96590b8c491050da0777c1ba707186137a
Craig Mayhew <craig@mayhew.io>
2024-02-21 10:38:51 +0000

    trunk can only be built with stable

--- cloudflare-build.sh
+++ cloudflare-build.sh
@@ -3,6 +3,6 @@ curl https://sh.rustup.rs -sSf | sh -s -- -y
 rustup default nightly
 rustup target add wasm32-unknown-unknown
 curl https://rustwasm.github.io/wasm-pack/installer/init.sh -sSf | sh -s -- -y
-cargo install --locked trunk
-cargo install wasm-bindgen-cli
+cargo +stable install --locked trunk
+cargo +stable install wasm-bindgen-cli
 trunk build
