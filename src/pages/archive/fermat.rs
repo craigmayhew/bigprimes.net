de3c0432d92b194644d8c83c232e9bdebabeadb4
Craig Mayhew <craig@mayhew.io>
2024-05-22 12:48:31 +0100

    refactor: remove unused Fermat struct field f

--- src/pages/archive/fermat.rs
+++ src/pages/archive/fermat.rs
@@ -8,7 +8,6 @@ mod fermat_utils {
     /// # Fermat struct
     pub struct Fermat<'a> {
         pub n: u64,
-        pub f: u64,
         pub digits: u64,
         pub prime_factors: &'a str,
     }
@@ -46,18 +45,18 @@ mod fermat_utils {
     /// The data to populate the Fermat structs is hard coded within this fn
     pub fn fermats<'a>() -> Vec<Fermat<'a>> {
         vec![
-			Fermat {n: 11, f: 0, digits:  617, prime_factors: "<a href=\"/cruncher/319489/\">P27567</a> × 974849 × 167988556341760475137 × 3560841906445833920513 × <a href=\"/cruncher/4093/\">P564</a>"},
-            Fermat {n: 10, f: 0, digits:  309, prime_factors: "45592577 × 6487031809 × 4659775785220018543264560743076778192897 × <a href=\"/cruncher/1601/\">P252</a>"},
-            Fermat {n:  9, f: 0, digits:  155, prime_factors: "2424833 × 7455602825647884208337395736200454918783366342657 × <a href=\"/cruncher/523/\">P99</a>"},
-            Fermat {n:  8, f: 0, digits:   78, prime_factors: "1238926361552897 × <a href=\"/cruncher/293/\">P62</a>"},
-            Fermat {n:  7, f: 0, digits:   39, prime_factors: "59649589127497217 × 5704689200685129054721"},
-            Fermat {n:  6, f: 0, digits:   20, prime_factors: "274177 × 67280421310721"},
-            Fermat {n:  5, f: 0, digits:   10, prime_factors: "<a href=\"/cruncher/641/\">P116</a> × 6700417"},
-            Fermat {n:  4, f: 0, digits:    5, prime_factors: "<a href=\"/cruncher/65537/\">P6543</a>"},
-            Fermat {n:  3, f: 0, digits:    3, prime_factors: "<a href=\"/cruncher/257/\">P55</a>"},
-            Fermat {n:  2, f: 0, digits:    2, prime_factors: "<a href=\"/cruncher/17/\">P7</a>"},
-            Fermat {n:  1, f: 0, digits:    1, prime_factors: "<a href=\"/cruncher/5/\">P3</a>"},
-            Fermat {n:  0, f: 0, digits:    1, prime_factors: "<a href=\"/cruncher/3/\">P2</a>"},
+			Fermat {n: 11, digits:  617, prime_factors: "<a href=\"/cruncher/319489/\">P27567</a> × 974849 × 167988556341760475137 × 3560841906445833920513 × <a href=\"/cruncher/4093/\">P564</a>"},
+            Fermat {n: 10, digits:  309, prime_factors: "45592577 × 6487031809 × 4659775785220018543264560743076778192897 × <a href=\"/cruncher/1601/\">P252</a>"},
+            Fermat {n:  9, digits:  155, prime_factors: "2424833 × 7455602825647884208337395736200454918783366342657 × <a href=\"/cruncher/523/\">P99</a>"},
+            Fermat {n:  8, digits:   78, prime_factors: "1238926361552897 × <a href=\"/cruncher/293/\">P62</a>"},
+            Fermat {n:  7, digits:   39, prime_factors: "59649589127497217 × 5704689200685129054721"},
+            Fermat {n:  6, digits:   20, prime_factors: "274177 × 67280421310721"},
+            Fermat {n:  5, digits:   10, prime_factors: "<a href=\"/cruncher/641/\">P116</a> × 6700417"},
+            Fermat {n:  4, digits:    5, prime_factors: "<a href=\"/cruncher/65537/\">P6543</a>"},
+            Fermat {n:  3, digits:    3, prime_factors: "<a href=\"/cruncher/257/\">P55</a>"},
+            Fermat {n:  2, digits:    2, prime_factors: "<a href=\"/cruncher/17/\">P7</a>"},
+            Fermat {n:  1, digits:    1, prime_factors: "<a href=\"/cruncher/5/\">P3</a>"},
+            Fermat {n:  0, digits:    1, prime_factors: "<a href=\"/cruncher/3/\">P2</a>"},
         ]
     }
 }
